7d2a2d38b1eb6e168605ae7d764deb8e510dc54d
郑MQ <1261647759@qq.com>
2021-10-29 22:34:49 +0800

    collection

---
+++ collection/collection/pages/market/market.js
@@ -0,0 +1,82 @@
+// pages/market/market.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    url:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2955645470,1731993861&fm=11&gp=0.jpg",
+    nickname:'匿名用户',
+    date:'2021/5/11',
+    time:'21:50',
+    items:'一块手表',
+    howlong:'5分钟前来过',
+    description:'全新，女朋友送的，打算送人;买一送一，顺带卖舍友;走过路过你不要错过，只要998元',
+    comments:'真的吗，只要，998，舍友带回家',
+    name:'陈一欣一'
+
+  },
+  /*getMyInfo: fuction(e) {
+    console.log(e.detail.userInfo)
+    let info=e.detail.userInfo;
+    this.setData({
+      name=info.nickName,
+      src=info.avatarUrl
+    })
+  },*/
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
